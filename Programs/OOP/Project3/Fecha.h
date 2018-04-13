6f57587a9d31cfc3654b0f31134e56b24397ebef
Loptt <carlos.deg02@hotmail.com>
2018-04-12 21:16:27 -0500

    Added class Fecha overload

--- Programs/OOP/Project3/Fecha.h
+++ Programs/OOP/Project3/Fecha.h
@@ -1,51 +1,149 @@
 
+
+#include <iostream>
+using namespace std;
+
 class Fecha
 {
+    // como friend la sobrecarga del operador >
+    friend bool operator > (Fecha f1, Fecha f2);
+
+    // como friend la sobrecarga del operador >=
+    friend bool operator >= (Fecha f1, Fecha f2);
+
+    // como friend la sobrecarga del operador <
+    friend bool operator < (Fecha f1, Fecha f2);
+
+    // como friend la sobrecarga del operador <=
+    friend bool operator <= (Fecha f1, Fecha f2);
+
+    // como friend la sobrecarga del operador ==
+    friend bool operator == (Fecha f1, Fecha f2);
+
+    // como friend la sobrecarga del operador +
+    // que recibe como parametro una fecha y un numero entero
+    // que representa una cantidad de dias y regresa
+    // la fecha a la que se le agregaron los dias indicados
+    friend Fecha operator + (Fecha f1, int dias);
+
+    // como friend la sobrecarga del operador >>
+    // que lee 3 valores enteros: dia, mes, anio con 4 digitos
+    friend istream operator >> ();
+
+    // como friend la sobrecarga del operador <<
+    // que muestra la fecha en el formato dd/mm/aaaa
+    // no incluyas espacios ni enter
+    friend ostream operator << ();
+
+
 public:
-    Fecha();
-    Fecha(int, int, int);
+    Fecha() { dd = 0; mm = 0; aa = 0; }
+    Fecha(int d, int m, int a) { dd = d; mm = m; aa = a; }
 
-    void setAa(int aa);
-    void setMm(int mm);
-    void setDd(int dd);
+    int getDia() { return dd; }
+    int getMes() { return mm; }
+    int getAnio() { return aa; }
 
-    Fecha operator+(int dias);
+    void setFecha(int d, int m, int a) { dd = d; mm = m; aa = a; }
 
 private:
+    // atributos
+    int dd, mm, aa;
 
-    int aa, mm, dd;
+    int calculaMaxDias();
+    bool esBiciesto();
 };
 
-Fecha::Fecha()
+bool Fecha::esBiciesto()
 {
-    aa = 2000;
-    mm = 0;
-    dd = 0;
+    return (!(aa % 4) && aa % 100 && !(aa % 400));
 }
 
-Fecha::Fecha(int dd, int mm, int aa)
+int Fecha::calculaMaxDias()
 {
-    this->dd = dd;
-    this->mm = mm;
-    this->aa = aa;
+    switch (mm)
+    {
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+            return 31;
+
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+            return 30;
+
+        case 2:
+
+            if(esBiciesto())
+            {
+                return 29;
+            }
+
+            return 28;
+            break;
+
+        default:
+
+            break;
+    }
 }
 
-void Fecha::setAa(int aa)
+bool friend operator==(Fecha f1, Fecha f2)
 {
-    this->aa = aa;
+    return f1.aa == f2.aa && f1.mm == f2.mm && f1.dd == f2.dd;
 }
 
-void Fecha::setMm(int mm)
+bool operator>(Fecha f1, Fecha f2)
 {
-    this->mm = mm;
+    return f1.aa > f2.aa && f1.mm > f2.mm && f1.dd > f2.dd;
 }
 
-void Fecha::setDd(int dd)
+bool operator>=(Fecha f1, Fecha f2)
 {
-    this->dd = dd;
+    return f1 > f2 || f1 == f2;
 }
 
-Fecha Fecha::operator+(int dias)
+bool operator<(Fecha f1, Fecha f2)
 {
-    if ()
+    return !(f1>f2);
+}
+
+bool operator<=(Fecha f1, Fecha f2)
+{
+    return f1 < f2 || f1 == f2;
+}
+
+Fecha operator+(Fecha f1, int diasAgregar)
+{
+    int maxDias = f1.calculaMaxDias();
+
+    int diasSobrantes;
+
+    do
+    {
+        diasSobrantes = diasAgregar - maxDias;
+
+        if (diasSobrantes > 0 )
+        {
+            if (++f1.mm > 12)
+            {
+                ++f1.aa;
+                f1.mm = 1;
+            }
+        }
+
+        maxDias = f1.calculaMaxDias();
+
+    }
+    while (diasSobrantes > 0);
+
+    return f1;
+
+
 }
