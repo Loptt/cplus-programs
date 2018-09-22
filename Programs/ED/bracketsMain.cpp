8e80cb914f60955298851889c888be6fe49c25c2
Carlos Estrada <carlos.deg02@hotmail.com>
2018-09-22 09:57:22 -0500

    Added brackets exercise

---
+++ Programs/ED/bracketsMain.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+struct Bracket
+{
+    char cBra;
+    int position;
+
+    Bracket(char c, int pos)
+    {
+        cBra = c;
+        position = pos;
+    }
+};
+
+bool checkForOpeningBracket(char character)
+{
+    switch (character)
+    {
+    case '(':
+        return true;
+
+    case '[':
+        return true;
+
+    case '{':
+        return true;
+    }
+
+    return false;
+}
+
+bool checkForClosingBracket(char character)
+{
+    switch (character)
+    {
+    case ')':
+        return true;
+
+    case ']':
+        return true;
+
+    case '}':
+        return true;
+    }
+
+    return false;
+}
+
+bool validateBracket(char lastBracket, char closingBracket)
+{
+    switch (lastBracket)
+    {
+    case '(':
+        if (closingBracket == ')')
+            return true;
+        return false;
+
+    case '[':
+        if (closingBracket == ']')
+            return true;
+        return false;
+
+    case '{':
+        if (closingBracket == '}')
+            return true;
+        return false;
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    stack<Bracket> brackets;
+    string input;
+
+    getline(cin, input);
+
+    for (int i = 0; i < input.length(); ++i)
+    {
+        if (checkForOpeningBracket(input[i]))
+        {
+            Bracket temp(input[i], i + 1);
+            brackets.push(temp);
+        }
+        else
+        {
+            if (checkForClosingBracket(input[i]))
+            {
+                if (!validateBracket(brackets.top().cBra, input[i]))
+                {
+                    cout << i + 1 << endl;
+                    return 0;
+                }
+
+                brackets.pop();
+            }
+        }
+    }
+
+    if (!brackets.empty())
+    {
+        cout << brackets.top().position << endl;
+        return 0;
+    }
+
+    cout << "Success" << endl;
+
+    return 0;
+}
