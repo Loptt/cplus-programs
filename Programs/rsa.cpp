81e45c18e26ffcb646c54780c3d5fe80955957ca
Carlos Estrada <carlos.deg02@hotmail.com>
2019-05-09 15:24:33 -0500

    Create rsa.cpp

---
+++ Programs/rsa.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int p = 13, q = 7;
+
+string encrypt(string msg)
+{
+    int n = p * q;
+}
+
+int main()
+{
+    string msg;
+    cin >> msg;
+
+    cout << encrypt(msg) << endl;
+    return 0;
+}
