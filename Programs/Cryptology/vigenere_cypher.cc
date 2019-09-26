d9247da5a773b04736741b92bb609ea5b02acc7d
Carlos Estrada <carlos.deg02@hotmail.com>
2019-09-25 20:07:46 -0500

    added vigenere

--- Programs/Cryptology/vigenere_cypher.cc
+++ Programs/Cryptology/vigenere_cypher.cc
@@ -1,33 +1,48 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 int main(int argc, char **argv) {
     if (argc != 3) {
-        std::cout << "Usage: " << argv[0] << " <-d (decrypt) | -e (encrypt)> <shift>" << std::endl;
+        std::cout << "Usage: " << argv[0] << " <-d (decrypt) | -e (encrypt)> <key>" << std::endl;
         return 1; 
     }
+
     bool de = argv[1][1] =='d';
-    int shift = atoi(argv[2]);
+
+    std::vector<int> shifts;
+
+    char key = argv[2][0];
+
+    for (int i = 1; key != '\0'; i++) {
+        shifts.push_back(key - 'A');
+        key = argv[2][i];
+    }
 
     if (de) {
-        shift = shift * -1;
+        for (int i = 0; i < shifts.size(); i++) {
+            shifts[i] = shifts[i] * -1;
+        }
     }
 
     std::string input;
+    int shiftIdx = 0;
 
     while (std::getline(std::cin, input)) {
         for (int i = 0; i < input.length(); i++) {
             if (input[i] == ' ') {
                 // Ignore
             } else {
-                char result = (char) (((input[i] - 'A' + shift) % 26) + 'A');
+                char result = (char) (((input[i] - 'A' + shifts[shiftIdx % shifts.size()]) % 26) + 'A');
                 if (result < 'A') {
                     result += 26;
                 }
                 std::cout << result;
+                shiftIdx++;
             }
         }
 
+        shiftIdx = 0;
         std::cout << std::endl;
     }
 
